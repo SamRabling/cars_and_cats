121ac3ae0fa0bba6b646c0b5215e24a05aa44ef2
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-02 12:43:14 -0700

    finished

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var server = http.createServer(function (request, response){
     console.log('client request URL:', request.url);
     if (request.url === '/') {
-        fs.readFile('index.html', 'utf8', function (errors, contents) {
+        fs.readFile('./views/index.html', 'utf8', function (errors, contents) {
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(contents);
             response.end();
@@ -17,7 +17,7 @@ var server = http.createServer(function (request, response){
         });
     }
     else if (request.url === '/cats') {
-        fs.readFile('./views/cars.html', 'utf8', function (errors, contents) {
+        fs.readFile('./views/cats.html', 'utf8', function (errors, contents) {
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(contents);
             response.end();
@@ -35,42 +35,42 @@ var server = http.createServer(function (request, response){
             response.writeHead(200, {'Content-Type': 'image/jpg'});
             response.write(contents);
             response.end();
-        })
+        });
     }
     else if (request.url === '/images/cat2.jpg') {
-        fs.readFile('./images/cat1.jpg', function (errors, contents) {
+        fs.readFile('./images/cat2.jpg', function (errors, contents) {
             response.writeHead(200, { 'Content-Type': 'image/jpg' });
             response.write(contents);
             response.end();
-        })
+        });
     }
     else if (request.url === '/images/cat3.jpg') {
-        fs.readFile('./images/cat1.jpg', function (errors, contents) {
+        fs.readFile('./images/cat3.jpg', function (errors, contents) {
             response.writeHead(200, { 'Content-Type': 'image/jpg' });
             response.write(contents);
             response.end();
-        })
+        });
     }
     else if (request.url === '/images/car1.jpg') {
-        fs.readFile('./images/cat1.jpg', function (errors, contents) {
+        fs.readFile('./images/car1.jpg', function (errors, contents) {
             response.writeHead(200, { 'Content-Type': 'image/jpg' });
             response.write(contents);
             response.end();
-        })
+        });
     }
     else if (request.url === '/images/car2.jpg') {
-        fs.readFile('./images/cat1.jpg', function (errors, contents) {
+        fs.readFile('./images/car2.jpg', function (errors, contents) {
             response.writeHead(200, { 'Content-Type': 'image/jpg' });
             response.write(contents);
             response.end();
-        })
+        });
     }
     else if (request.url === '/images/car3.jpg') {
-        fs.readFile('./images/cat1.jpg', function (errors, contents) {
+        fs.readFile('./images/car3.jpg', function (errors, contents) {
             response.writeHead(200, { 'Content-Type': 'image/jpg' });
             response.write(contents);
             response.end();
-        })
+        });
     }
     else {
         response.writeHead(404);
